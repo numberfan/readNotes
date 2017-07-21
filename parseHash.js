ef83f71db9b69d047f206ceb0e88ecefe6af0633
shirychen <chenfan@dayuw.com>
2017-07-21 17:32:30 +0800

    获取url hash值

--- parseHash.js
+++ parseHash.js
@@ -25,3 +25,15 @@ var parseUrl = function(url) {
     return reArr;
 }
 
+
+//使用getParams("content-id")
+ function getParams(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    }
+    return null;
+}
+
+
